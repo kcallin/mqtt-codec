c27aa256d0266f258dc02720a3212eab8158e652
Keegan Callin <kc@kcallin.net>
2018-11-17 20:15:27 -0700

    Updated setup.py calls and documentation.

--- setup.py
+++ setup.py
@@ -38,14 +38,40 @@ chdir(project_dir)
 setup(
     name="mqtt-codec",
     version="0.1.3",
+    # Want to specify opt-in versions but found that when using
+    # pip 9.0.3 (who knows what other versions), the comma seems to
+    # prevent any part of the string from being recognized.
+    #
+    # python_requires='==2.7.*,==3.4.*,==3.5.*,==3.6.*,==3.7.*',
+    #
+    python_requires='>=2.7',
     install_requires=install_requires,
     tests_require=[],
-    classifiers=[  # Optional
+    use_2to3=True,
+    packages=['mqtt_codec'],
+    test_suite="tests",
+    author="Keegan Callin",
+    author_email="kc@kcallin.net",
+    # license param is used when the license is not specified as a trove
+    # classifier. According to note (5) at
+    #
+    #   Writing the Setup Script, Note 5, https://docs.python.org/3/distutils/setupscript.html#additional-meta-data
+    #     Retrieved 2018-11-17.
+    #
+    url="https://github.com/kcallin/mqtt-codec",  # project home page
+    description="Weapons grade MQTT packet codec.",
+    long_description=read_path('README.rst'),
+    project_urls={
+        "Bug Tracker": "https://github.com/kcallin/mqtt-codec/issues",
+        "Documentation": "https://mqtt-codec.readthedocs.io/en/latest/",
+        "Source Code": "https://github.com/kcallin/mqtt-codec",
+    },
+    classifiers=[
         # How mature is this project? Common values are
         #   3 - Alpha
         #   4 - Beta
         #   5 - Production/Stable
-        'Development Status :: 3 - Alpha',
+        'Development Status :: 4 - Beta',
 
         # Indicate who your project is intended for
         'Intended Audience :: Developers',
@@ -58,34 +84,11 @@ setup(
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
         'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
         'License :: OSI Approved :: GNU Lesser General Public License v3 (LGPLv3)',
         'Operating System :: POSIX :: Linux',
     ],
-    test_suite="tests",
-    use_2to3=True,
-    packages=['mqtt_codec'],
-    author="Keegan Callin",
-    author_email="kc@kcallin.net",
-    # license is used when the license is not specified as a trove
-    # classifier.According to note (5) at
-    #
-    #   Writing the Setup Script, Note 5, https://docs.python.org/3/distutils/setupscript.html#additional-meta-data
-    #     Retrieved 2018-11-17.
-    # could also include download_url, etc.
-    url="https://github.com/kcallin/mqtt-codec",  # project home page
-    description="Weapons grade MQTT packet codec.",
-    long_description=read_path('README.rst'),
-    project_urls={
-        "Bug Tracker": "https://github.com/kcallin/mqtt-codec/issues",
-        "Documentation": "https://mqtt-codec.readthedocs.io/en/latest/",
-        "Source Code": "https://github.com/kcallin/mqtt-codec",
-    },
-    # Want to specify opt-in versions but found that when using
-    # pip 9.0.3 (who knows what other versions), the comma seems to
-    # prevent any part of the string from being recognized.
-    #
-    # python_requires='==2.7.*,==3.6.*',
-    #
-    python_requires='>=2.7',
 )
