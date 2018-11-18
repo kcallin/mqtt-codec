9842bf557de10fe9b4a651b5d3a59ce34cb3c773
Keegan Callin <kc@kcallin.net>
2018-11-18 08:48:54 -0700

    setup.py version 0.1.3 -> 0.1.4.

--- setup.py
+++ setup.py
@@ -37,7 +37,7 @@ project_dir = abspath(dirname(__file__))
 chdir(project_dir)
 setup(
     name="mqtt-codec",
-    version="0.1.3",
+    version="0.1.4",
     # Want to specify opt-in versions but found that when using
     # pip 9.0.3 (who knows what other versions), the comma seems to
     # prevent any part of the string from being recognized.
