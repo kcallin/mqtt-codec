3e4a66ec7c080dc6b48687d438dc076b12a7061c
Keegan Callin <kc@kcallin.net>
2018-11-17 23:23:40 -0700

    Documentation version set to 0.1.3.

--- doc/README.rst
+++
@@ -1,3 +0,0 @@
-
-http://blockdiag.com/en/seqdiag/examples.html
-pip install sphinxcontrib-seqdiag
--- doc/source/conf.py
+++ doc/source/conf.py
@@ -24,9 +24,9 @@ copyright = u'2018, Keegan Callin'
 author = u'Keegan Callin'
 
 # The short X.Y version
-version = u'1.0.0'
+version = u'0.1.3'
 # The full version, including alpha/beta/rc tags
-release = u'1.0.0'
+release = u'0.1.3'
 
 
 # -- General configuration ---------------------------------------------------
